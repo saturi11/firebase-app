3e29032c4c47826ee0a78fbbe7cc904f34c22ee3
Gabriel Saturi <gabrielsaturi11@gmail.com>
2024-08-05 15:17:47 -0300

    chore: Add Firebase Remote Config for dynamic app configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase/app.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
@@ -9,5 +10,17 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  final remoteConfig = FirebaseRemoteConfig.instance;
+  await remoteConfig.setConfigSettings(
+    RemoteConfigSettings(
+      fetchTimeout: const Duration(seconds: 10),
+      minimumFetchInterval: const Duration(hours: 1),
+    ),
+  );
+  await remoteConfig.setDefaults(const {
+    'app_name': 'Flutter Firebase',
+    'primary_color': 0xFF2196F3,
+  });
+
   runApp(const App());
 }
